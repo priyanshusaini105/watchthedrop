513bcc43ff2d6f7df150aae796b0bffe49bde8e1
Piyanshu Saini <priyanshusaininew@gmail.com>
2024-01-20 11:02:57 +0530

    fix: is by url

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,35 +4,17 @@ import { ProductInfo } from '@/types';
 import ProductCard from '@/components/productcard/ProductCard';
  //import { useSearchParams } from 'next/navigation'
 
-async function Products() {
+async function Products({searchParams}:{searchParams:{q:string}}) {
 // const searchParams = useSearchParams();
-// const q = searchParams.get("q");
-// console.log(q);
-const res = await fetch(`https://dropmytest.onrender.com/search?q=iphone`);
+
+const res = await fetch(`https://dropmytest.onrender.com/search?q=${searchParams.q}`);
+
 const data = await res.json() as {item: ProductInfo[]};
 
 
   return (
     <div className='container mt-24 w-full items-center justify-center flex flex-wrap gap-24 '>
         {
-            data.item.map((item) => {
-                return (
-                    <div className="post" key={item.product_id}>
-                        <ProductCard product={item}/>
-                    </div>
-                )
-            })
-        }
-         {
-            data.item.map((item) => {
-                return (
-                    <div className="post" key={item.product_id}>
-                        <ProductCard product={item}/>
-                    </div>
-                )
-            })
-        }
-         {
             data.item.map((item) => {
                 return (
                     <div className="post" key={item.product_id}>
